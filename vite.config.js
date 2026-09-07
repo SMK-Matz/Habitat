feb022c4c366d13afd5843136cb04b1b116f9160
SMK-Matz <mbeuchel@smk.nl>
2026-09-07 15:07:23 +0200

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // change base to '/your-repo-name/'  e.g. '/habitat-inventory/'
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/Habitat/',
 })
